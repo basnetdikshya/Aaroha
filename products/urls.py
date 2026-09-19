ce6fc54fb78c5d625d0bc8a5d0ce96d74b10cccb
basnetdikshya <basnetdikshya950@gmail.com>
2026-09-19 11:58:37 +0545

    Updated code

--- products/urls.py
+++ products/urls.py
@@ -3,10 +3,27 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.product_list, name='products'),
+     path(
+        '',
+        views.product_list,
+        name='products'
+    ),
+
+    path(
+        'inventory/',
+        views.inventory,
+        name='inventory'
+    ),
+
     path(
         '<int:pk>/',
         views.product_detail,
         name='product_detail'
+    ),
+
+       path(
+        'update-stock/<int:pk>/',
+        views.update_stock,
+        name='update_stock'
     ),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Product
+from django.contrib.auth.decorators import login_required
+from accounts.models import UserProfile
+
 
 
 def product_list(request):
@@ -30,3 +33,73 @@ def product_detail(request, pk):
         'pages/product_detail.html',
         {'product': product}
     )
+
+@login_required
+def inventory(request):
+
+    # Superuser can access
+    if request.user.is_superuser:
+        products = Product.objects.all().order_by('name')
+
+    else:
+
+        # Check user role
+        try:
+            profile = UserProfile.objects.get(
+                user=request.user
+            )
+
+        except UserProfile.DoesNotExist:
+            return redirect('login')
+
+        # Only owner and staff can access inventory
+        if profile.role not in ['owner', 'staff']:
+            return redirect('login')
+
+        products = Product.objects.all().order_by('name')
+
+    # Inventory statistics
+    total_products = products.count()
+
+    in_stock = products.filter(
+        stock__gt=10
+    ).count()
+
+    low_stock = products.filter(
+        stock__gt=0,
+        stock__lte=10
+    ).count()
+
+    out_of_stock = products.filter(
+        stock=0
+    ).count()
+
+    context = {
+        'products': products,
+        'total_products': total_products,
+        'in_stock': in_stock,
+        'low_stock': low_stock,
+        'out_of_stock': out_of_stock,
+    }
+
+    return render(
+        request,
+        'products/inventory.html',
+        context
+    )
+
+def update_stock(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        stock = request.POST.get('stock')
+
+        if stock:
+            product.stock = stock
+            product.save()
+
+        return redirect('inventory')
+
+    return render(request, 'pages/update_stock.html', {
+        'product': product
+    })
